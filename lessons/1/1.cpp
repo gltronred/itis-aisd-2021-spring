4a877aaaef7f5730871f6e075b16eacf3152f2b8
Mansur Ziiatdinov <gltronred@gmail.com>
2021-02-18 16:19:56 +0300

    Решение задачи про простоту

--- lessons/1/1.cpp
+++ lessons/1/1.cpp
@@ -31,7 +31,12 @@ void print() {
 // отрицательные - не простые
 // примеры простых: 2, 3, 5, 7, 11, 13, 17, 19
 bool isPrime(int n) {
-  return false;
+  if (n<2) return false;
+  for (int i=2; i*i<=n; i++) {
+    if (n % i == 0)
+      return false;
+  }
+  return true;
 }
 
 // точка входа
