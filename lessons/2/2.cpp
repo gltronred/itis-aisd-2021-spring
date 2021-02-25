a20554b070e6fe4d562eef9e7da86b4e17fc5f65
Mansur Ziiatdinov <gltronred@gmail.com>
2021-02-25 16:07:55 +0300

    Задание: сумма элементов списка

--- lessons/2/2.cpp
+++ lessons/2/2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 struct elem {
   int val;
   elem* next;
@@ -19,13 +21,24 @@ elem* empty_list() {
 }
 
 void print_list(elem* head) {
+  elem* p = head;
+  while (p != NULL) {
+    cout << p->val << " ";
+    p = p->next;
+  }
+  cout << endl;
+}
+
+// Возвращает сумму элементов списка
+int sum_list(elem* head) {
+ 
 }
 
 int main() {
   elem* head = empty_list();
   head = add(head, 1);
   head = add(head, 3);
-  head = add(head, -123);
+  head = add(head, -124);
   print_list(head);
   return 0;
 }
