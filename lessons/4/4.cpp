ecd6b5d0794ad4192e3ba87208e2ac6b5baca785
Mansur Ziiatdinov <gltronred@gmail.com>
2021-03-11 16:46:37 +0300

    Решение

--- lessons/4/4.cpp
+++ lessons/4/4.cpp
@@ -4,8 +4,60 @@
 
 using namespace std;
 
+struct elem {
+  int val;
+  elem* next;
+};
+
+// & - чтобы можно было менять аргумент
+// (передача по ссылке)
+void push(elem*& head, int n) {
+  elem *p = new elem;
+  p->val = n;
+  p->next = head;
+  head = p;
+}
+
+int pop(elem*& head) {
+  elem *p = head;
+  head = head->next;
+  int n = p->val;
+  delete p;
+  return n;
+}
+
+bool empty(elem* head) {
+  return head == NULL;
+}
+
 int compute(int n, const char* const expr) {
-  return 0;
+  elem* head = NULL;
+  for (int i=0; i<n; i++) {
+    int a,b;
+    switch(expr[i]) {
+      case '+':
+        a = pop(head);
+        b = pop(head);
+        push(head, b+a);
+        break;
+      case '-':
+        a = pop(head);
+        b = pop(head);
+        push(head, b-a);
+        break;
+      case '*':
+        a = pop(head);
+        b = pop(head);
+        push(head, b*a);
+        break;
+      case ' ':
+        break;
+      default:
+        a = expr[i] - '0';
+        push(head, a);
+    }
+  }
+  return pop(head);
 }
 
 int main() {
