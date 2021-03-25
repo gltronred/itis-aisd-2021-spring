eb68023696138e6728231a33d13e0b45c5c7b742
Mansur Ziiatdinov <gltronred@gmail.com>
2021-03-25 16:48:50 +0300

    Решение

--- lessons/6/6.cpp
+++ lessons/6/6.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <queue>
 
 using namespace std;
 
@@ -14,19 +15,48 @@ int main() {
     for (int j=0; j<m; j++) {
       int x;
       cin >> x;
-      a[i].push_back(x);
+      a[i].push_back(x-1);
     }
   }
 
   // a[i] - список вершин, связанных с i-й
   // ваше решение тут
-  // ...
+  bool f=false;
+  int visited[n];
+  for (int i=0; i<n; i++) visited[i]=0;
+  queue<int> q;
+  int k=0;
+  do {
+    q.push(k);
+    visited[k]=1;
 
-  for (int i=0; i<n; i++) {
-    for (auto it = a[i].begin(); it != a[i].end(); ++it) {
-      cout << *it << " ";
+    while (!q.empty()) {
+      k=q.front();
+      q.pop();
+      for (auto it=a[k].begin(); it!=a[k].end(); ++it) {
+        int x = *it;
+        f |= (visited[x] == visited[k]);
+        if (visited[x]==0) {
+          q.push(x);
+          visited[x]=3-visited[k];
+        }
+      }
     }
-    cout << endl;
+
+    k=0;
+    while(k<n && visited[k]!=0) k++;
+  } while (!f && k<n);
+
+  if (!f) {
+    cout << "YES" << endl;
+    for (int d=1; d<3; d++) {
+      for (int i=0; i<n; i++)
+        if (visited[i]==d)
+          cout << (i+1) << " ";
+      cout << endl;
+    }
+  } else {
+    cout << "NO" << endl;
   }
 
   return 0;
